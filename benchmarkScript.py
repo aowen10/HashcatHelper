b8a169ec18e20382ba6f7d2232d6517bc2452a8d
Alex Owen <aowen14@gmail.com>
2020-01-15 16:07:30 -0800

    add outputFile

--- benchmarkScript.py
+++ benchmarkScript.py
@@ -2,11 +2,12 @@
 import subprocess
 
 hashcodeFile = './listofhashcodes'
+outputFile = 'outputFile.txt'
 
 hashcodeList = [line.rstrip('/n') for line in open(hashcodeFile)]
 
 basecommand = 'hashcat -b -O -m '
-
+postcommand = ' >> ' + outputFile
 for code in hashcodeList :
-    bashcommand = basecommand + code
+    bashcommand = basecommand + code + postcommand
     subprocess.call(bashcommand.split())
