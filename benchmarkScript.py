ad21cf33741dd676ae0695ecc9c4f0924d10c667
Alex Owen <aowen14@gmail.com>
2020-01-17 15:34:59 -0800

    v1 of Human readable version

--- benchmarkScript.py
+++ benchmarkScript.py
@@ -1,13 +1,34 @@
 
 import subprocess
 
-hashcodeFile = './listofhashcodes'
+
+hashcodeFile = 'listofhashcodes.txt'
+intermediateFile='intermediateFile'
 outputFile = 'outputFile.txt'
 
-hashcodeList = [line.rstrip('/n') for line in open(hashcodeFile)]
+hashcodeList = [line.rstrip('\n') for line in open(hashcodeFile)]
 
 basecommand = 'hashcat -b -O -m '
-postcommand = ' >> ' + outputFile
-for code in hashcodeList :
-    bashcommand = basecommand + code + postcommand
-    subprocess.call(bashcommand.split())
+with open(outputFile, "w") as output_f:
+    for code in hashcodeList :
+        bashcommand = basecommand + code
+        print("Executing: " + bashcommand) 
+        print("code: " + '0')
+        
+
+        proc = subprocess.Popen(bashcommand.split(),stdout= subprocess.PIPE)
+        std_out = proc.stdout.read().decode("utf-8")
+
+        splitted = std_out.splitlines()
+        pastHashmode = False
+        for spl in splitted :
+            if not "Hashmode" in spl and not pastHashmode:
+                continue
+            pastHashmode = True
+            if "Started" in spl :
+                break
+            output_f.write(spl + '\n')
+output_f.close()
+
+    
+
